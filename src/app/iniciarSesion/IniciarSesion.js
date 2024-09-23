776710316e2e60d36b2e8aff793873caf42bad3f
¨Siwar <karonisiwar@gmail.com>
2024-09-23 10:33:56 +0200

    estilo

--- src/app/iniciarSesion/IniciarSesion.js
+++ src/app/iniciarSesion/IniciarSesion.js
@@ -1,42 +1,44 @@
 import React from "react";
 import style from "./IniciarSesion.module.css";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function IniciarSesion() {
   return (
     <div className={style.body}>
       <div className={style.formContainer}>
-        <Image
-          className={style.logo}
-          src="/images/Logo.jpg"
-          alt="logo"
-          width={120}
-          height={120}
-          layout="intrinsic"
-        />
         <form action="/registro.php" id="formulario-registro">
-          <legend className={style.legend}>Inicia Sesión</legend>
-          <label htmlFor="correo" className={style.label}>Correo Electrónico:</label>
-          <input type="email" id="correo" name="correo" className={style.input} required />
-          <label htmlFor="password" className={style.label}>Contraseña:</label>
-          <input type="password" id="password" name="password" className={style.input} required />
-          <Link href="/perfilPrivado">
-            <input className={style.btn} type="submit" value="Enviar" />
+          <legend className={style.legend}>Login</legend>
+          <label htmlFor="correo" className={style.label}>
+            Email Address
+          </label>
+          <input
+            type="email"
+            id="correo"
+            name="correo"
+            className={style.input}
+            required
+          />
+          <label htmlFor="password" className={style.label}>
+            Enter password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className={style.input}
+            required
+          />
+          <Link href="/perfilPrivado/listadoParques">
+            <input className={style.btn} type="submit" value="Login" />
           </Link>
-          <p className={style.link}>¿No tienes cuenta?</p>
+          <Link href="/forgot-password" className={style.link}>
+            Forgot Password?
+          </Link>
+          <p className={style.link}>Don't have an account?</p>
           <Link href="/registro" className={style.link}>
-            Regístrate
+            Create Account
           </Link>
         </form>
-        <div className={style.cardImage}>
-          <Image
-            src="/images/imagenAbajoDeReistro.png"
-            alt="Imagen de registro"
-            width={300}
-            height={200}
-          />
-        </div>
       </div>
     </div>
   );
